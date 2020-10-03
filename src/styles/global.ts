85682ce092e365ce58f79055f988e7385042b54b
wiliamvj <wiliamjoaquim@gmail.com>
2020-10-03 17:16:44 -0300

    finish style dashboard

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,7 +12,7 @@ export default createGlobalStyle`
 
 body {
   background: #F0F0F5 url(${githubBackground}) no-repeat 70% top;
-  -webkit-font-smoothin: antialiased;
+  -webkit-font-smoothing: antialiased;
 }
 
 body, input, button {
