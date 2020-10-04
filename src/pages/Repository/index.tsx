9535784318c02a7b9fd03fb2e7c5c3542375527c
wiliamvj <wiliamjoaquim@gmail.com>
2020-10-04 15:46:20 -0300

    add styles to repository

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +13,55 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars2.githubusercontent.com/u/30341785?v=4"
+            alt="wiliamvj"
+          />
+          <div>
+            <strong>Test/teste</strong>
+            <p>Description</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>255</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>58</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>98</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to={`gdg`}>
+          <div>
+            <strong>etet</strong>
+            <p>teste</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
